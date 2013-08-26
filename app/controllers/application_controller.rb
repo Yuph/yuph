7faebd58e2fd17253a1b681a9265f1302d0d5425
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2013-08-26 17:46:51 -0300

    added before_filter authenticate and set_session_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate
+
+  def authenticate
+		redirect_to :controller => "session", :action => "index" if session[:user] == nil
+	end
+  def set_session_user
+  	@session_user = User.find(session[:user])
+  end
 end
