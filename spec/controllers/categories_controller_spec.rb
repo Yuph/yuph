c7af57684f91dd92cdc72d05d5a71c45d3cd0a44
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2013-09-02 16:56:24 -0300

    added test if user can add category on this forum

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -8,7 +8,7 @@ describe CategoriesController do
       @idea = FactoryGirl.create(:idea)
       @idea.users << @user
       @idea.save!
-      @category = FactoryGirl.create(:forum, idea_id: @idea.id)
+      @forum = FactoryGirl.create(:forum, idea_id: @idea.id)
     end
     context "#GET" do
       it "Render 'new' Template" do
@@ -16,7 +16,7 @@ describe CategoriesController do
         expect(response).to render_template(:new)
       end
       it "get one forum" do
-        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @category.id)
+        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @forum.id)
         get :show, id: Category.last.id
         expect(response).to render_template(:show)
       end
@@ -24,26 +24,31 @@ describe CategoriesController do
     context "#POST" do
       it "Succefuly" do
         expect{
-          post :create, category: FactoryGirl.attributes_for(:category, forum_id: @category.id)
+          post :create, category: FactoryGirl.attributes_for(:category, forum_id: @forum.id)
         }.to change(Category,:count).by(1)
       end
       it "Fail" do
-        post :create, category: FactoryGirl.attributes_for(:category, title: "", forum_id: @category.id)
+        post :create, category: FactoryGirl.attributes_for(:category, title: "", forum_id: @forum.id)
         expect(response).to render_template(:new)
       end
     end
     context "#PUT" do
       it "Succefuly" do
-        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @category.id)
+        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @forum.id)
         category = Category.last
-        put :update, id: category.id, category: FactoryGirl.attributes_for(:category, title: "changed", forum_id: @category.id)
+        put :update, id: category.id, category: FactoryGirl.attributes_for(:category, title: "changed", forum_id: @forum.id)
         category.reload
         expect(category.title).to eql("changed")
+      end
+      it "Fail" do
+        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @forum.id)
+        put :update, id: Category.last.id, category: FactoryGirl.attributes_for(:category, title: "", forum_id: @forum.id)
+        expect(response).to render_template(:edit)
       end
     end
     context "#DELETE" do
       it "Succefuly" do
-        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @category.id)
+        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @forum.id)
         category = Category.last
         expect{
           delete :destroy, id: category.id
@@ -52,13 +57,19 @@ describe CategoriesController do
     end
     context "#ASSHOLE" do
       it "#MANAGE one forum that not belongs to me" do
-        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @category.id)
+        post :create, category: FactoryGirl.attributes_for(:category, forum_id: @forum.id)
         category = Category.last
         user = FactoryGirl.create(:user)
         session[:user] = user.id
         expect{
           delete :destroy, id: category.id
         }.to change(Category,:count).by(0)
+      end
+      it "#MANAGE one idea that not belongs to me" do
+        idea = FactoryGirl.create(:idea)
+        forum = FactoryGirl.create(:forum, idea_id: idea.id)
+        post :create, category: FactoryGirl.attributes_for(:category, forum_id: forum.id)
+        expect(response).to redirect_to(action: :index)
       end
     end
   end
