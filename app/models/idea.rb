34d58a616d78b20abb2b66f8f04fe306ee6d9cb0
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2013-10-18 00:09:33 -0300

    Added default image in idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -16,15 +16,13 @@ class Idea < ActiveRecord::Base
   has_attached_file :image,
   	:styles => { :medium => "300x300>", :thumb => "220x144#" },
     :storage => :s3,
+    :default_url => '/assets/missing.png',
     :bucket => 'yuph',
     :path => "idea/:attachment/:id/:style.:extension",
     :s3_credentials => { :access_key_id => 'AKIAI5MJFU42WK57XFEQ',
                          :secret_access_key => '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' },
     :s3_permissions => 'public-read',
     :s3_host_name => 's3-us-west-2.amazonaws.com'
-
-  #validates_attachment :image, :presence => true,
-     #:content_type => { :content_type => /^image\/(png|gif|jpeg)/ } if Rails.env != "test"
 
   scope :last_four, -> {self.last(4)}
 
