c4edcdc15bafcd3e83e2d78b1805ff2fa470b265
Naiane Garcia Motta <naiane@naiane-notebook.(none)>
2013-10-21 00:28:31 -0200

    adicionado seed.rb][

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = User.create(nick: "estagiário", email: "estagiario@yuph.net", password: "53245324")
-idea = Idea.create(name: "Yuph", mini_description: "Um ambiente em que qualquer ideia pode se tornar algo incrível.", description: "", image_file_name: "/assets/big-logo.png")
+idea = Idea.create(name: "Yuph", mini_description: "Um ambiente em que qualquer ideia pode se tornar algo incrível.", description: "Siga-nos", image_file_name: "/assets/big-logo.png")
 idea.users << user
 idea.save!
 Forum.create!(idea: idea)
