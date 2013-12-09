0a83362199b923bf9d2628d422f0df45993429e7
Naiane Garcia Motta <naiane@naiane-notebook.(none)>
2013-12-08 22:52:39 -0200

    adicionada categorias default para os forums

---
+++ db/migrate/20131209004728_add_default_category_to_ideas.rb
@@ -0,0 +1,13 @@
+class AddDefaultCategoryToIdeas < ActiveRecord::Migration
+  def up
+  	Forum.find_each do |forum|
+  	  forum.categories.build(:title => "Off-topic").save!
+      forum.categories.build(:title => "Sugestões").save!
+      forum.categories.build(:title => "Brainstorm").save!
+      forum.categories.build(:title => "Geral").save!
+  	end
+  end
+
+  def down
+  end
+end
