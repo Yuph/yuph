dd18eee0ef3cb475b0fa4b152c417e79904e134b
Lucas Zawacki <lfzawacki@gmail.com>
2014-04-05 19:25:49 -0300

    Filtering notifications for the current_user

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,15 @@
 class NotificationsController < ApplicationController
 
   def index
-    @activities = PublicActivity::Activity.all
+    act = PublicActivity::Activity
+    # User owned activity
+    @activities = act.where(:owner => current_user)
+    # Activity in user ideas
+    @activities |= act.where(:trackable_type => 'Idea', :trackable_id => current_user.ideas)
+    # Activity in user's ideas forum
+    @activities |= act.where(:trackable_type => 'Forum', :trackable_id => current_user.ideas.map(&:forum))
+    # Activity in user's followed ideas
+    @activities |= act.where(:trackable_type => 'Idea', :trackable_id => current_user.follows.map(&:idea))
   end
 
 end
