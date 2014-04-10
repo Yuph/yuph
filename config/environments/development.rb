40e3b52d369f5f16723b899d403038f0a6eaa01d
Lucas Zawacki <lfzawacki@gmail.com>
2014-04-10 00:51:19 -0300

    New code for analytics

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Yuph::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Analytics
+  GA.tracker = ENV['GA_TRACKER_CODE']
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Yuph::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  GA.tracker = "UA-44206961-1"
+  GA.tracker = ENV['GA_TRACKER_CODE']
 end
