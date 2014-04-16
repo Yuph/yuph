35087925dfdbe2452bace7477feb39efbddb8967
Lucas Zawacki <lfzawacki@gmail.com>
2014-04-15 22:23:23 -0300

    Correct db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,7 +7,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(nick: "estagiário", email: "estagiario@yuph.net", password: "53245324")
+user = User.create(email: "estagiario@yuph.net", password: "53245324")
+user.profile = Profile.create nick: "estagiario"
 puts " * Create user #{user.nick}"
 
 idea = Idea.create(name: "Yuph", mini_description: "Um ambiente em que qualquer ideia pode se tornar algo incrível.", description: "Siga-nos", image_file_name: "/assets/big-logo.png")
